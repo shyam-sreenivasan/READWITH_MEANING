b05599c48caa440b2eb3559de1d63273bc4d7de1
Shyam Sreenivasan <shyamsreeni.612@gmail.com>
2022-11-04 20:13:53 +0530

    moved meaning table to right

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import read_with_meaning as rwm
 
 if __name__ == '__main__':
-    rwm.app.run('0.0.0.0', port=8000, debug=False, threaded=True)
+    rwm.app.run('0.0.0.0', port=8000, debug=True, threaded=True)
 
